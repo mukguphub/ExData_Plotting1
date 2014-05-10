8f1fe3fdc6852847c1741651c3f266ff898df0c7
Mukesh Gupta <mukesh.gupta@onstar.com>
2014-05-10 02:31:36 -0400

    plot2 working code

---
+++ plot2.R
@@ -0,0 +1,12 @@
+#Read Data Function
+data<- read.csv("household_power_consumption.txt",sep=";",na.strings = "?",stringsAsFactors=FALSE)
+data[,1] <- as.Date(data[,1],format="%d/%m/%Y")
+data <- data[data$Date %in% as.Date(c('2007-02-01',' 2007-02-02')),]
+data$Time <- paste(data$Date,data$Time,sep=" ")
+data$Time <- as.POSIXct(data$Time,"%Y-m%-d% %H:%M:%S") 
+
+png("plot2.png",width = 480,height = 480,units = "px",bg="transparent")
+
+plot(data$Time,data[,3], type="l", ylab="Global Active Power (kilowatts)", xlab="")
+
+dev.off()
